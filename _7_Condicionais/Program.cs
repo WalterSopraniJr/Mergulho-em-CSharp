c747a071ec007a07fa863331072274df79c6505b
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-05-12 06:18:28 -0300

    Acrescentado condicionais.

--- _7_Condicionais/Program.cs
+++ _7_Condicionais/Program.cs
@@ -13,16 +13,27 @@ namespace _7_Condicionais
             Console.WriteLine("Executando Projeto 7 - Condicionais");
             Console.WriteLine("-----------------------------------");
 
-            int idadeJoao = 16;
+            int idade = 16;
+            int quantidadePessoas = 3;
 
-            if(idadeJoao >= 18)
+            if (idade >= 18)
             {
-                Console.WriteLine("João é maior de idade.");
-            } else
-            {
-                Console.WriteLine("João ainda é menor de idade");
+                Console.WriteLine("Você tem mais que 18 anos");
+                Console.WriteLine("Seja bem vindo");
             }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("voce nao tem 18, mas pode entrar, pois está acompanhado");
 
+                }
+                else
+                {
+                    Console.WriteLine("infelizmente voce nao pode entrar");
+                }
+
+            }
             Console.WriteLine("-----------------------------------");
             Console.WriteLine("Execução Finalizada. Tecle enter para sair...");
             Console.ReadLine();
