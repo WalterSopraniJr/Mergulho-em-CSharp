e3eeafcbcf3dd14988524afdd1d88cee14735b4a
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-05-13 05:52:42 -0300

    Corrigido erros em condicionais 2.

--- 8_Condicionais2/Program.cs
+++ 8_Condicionais2/Program.cs
@@ -10,6 +10,24 @@ namespace _8_Condicionais2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Executando Projeto 8 - _8_Condicionais2 2");
+
+            int idade = 16;
+            int quantidadePessoas = 3;
+
+            if (idade >= 18 || quantidadePessoas >= 2)
+            {
+                Console.WriteLine("Seja bem vindo");
+            }
+            else
+            {
+                Console.WriteLine("infelizmente voce nao pode entrar");
+            }
+
+        
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Execução Finalizada. Tecle enter para sair...");
+            Console.ReadLine();
         }
     }
 }
