c0deec1331353b76df915b2dba548f2fc72465a1
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-05-13 06:10:48 -0300

    Aplicando Escopo de variáveis.

--- 9_Escopo/Program.cs
+++ 9_Escopo/Program.cs
@@ -4,20 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _8_Condicionais2
+namespace _9_Escopo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Executando Projeto 8 - Condicionais 2");
+            Console.WriteLine("Executando Projeto 9 - Escopo");
+            Console.WriteLine("-----------------------------------");
 
             int idade = 18;
-            int quantidadePessoas = 3;
-            bool confirmaIdade = idade >= 18;
-            bool acompanhado = quantidadePessoas >= 2;
+            bool acompanhado = false;
+            String mensagemAdicional;
 
-            if (confirmaIdade == true && acompanhado == true)
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado!";
+                Console.WriteLine(mensagemAdicional);
+            } else
+            {
+                mensagemAdicional = "João está desacompanhado";
+                Console.WriteLine(mensagemAdicional);
+            }
+
+            if (idade >= 18 && acompanhado)
             {
                 Console.WriteLine("Seja bem vindo");
             }
@@ -26,7 +36,7 @@ namespace _8_Condicionais2
                 Console.WriteLine("infelizmente voce nao pode entrar");
             }
 
-        
+
             Console.WriteLine("-----------------------------------");
             Console.WriteLine("Execução Finalizada. Tecle enter para sair...");
             Console.ReadLine();
