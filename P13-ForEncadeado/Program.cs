ba16de13caf9097f57985c69be2ad7250884e897
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-05-14 19:25:11 -0300

    Criando uma árvore de asteríscos com laços de 'For' e utilizando break.

---
+++ P13-ForEncadeado/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P13_ForEncadeado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 13 - Encadeamento de For");
+            Console.WriteLine("-----------------------------------");
+
+            for(int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
+            {
+                for(int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
+                {
+                    Console.Write("*");
+                    if(contadorColuna >= contadorLinha)
+                        break;
+                    
+                }
+                Console.WriteLine();
+
+            }
+
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Execução Finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
