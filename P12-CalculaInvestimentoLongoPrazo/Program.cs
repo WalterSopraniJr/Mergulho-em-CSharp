79c3f36bcd6ca6261c44af8870b55624db21de55
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-05-14 19:13:13 -0300

    Calculando investimento a longo prazo encadeando laços de repetição 'For'.

---
+++ P12-CalculaInvestimentoLongoPrazo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P12_CalculaInvestimentoLongoPrazo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 12 - Calcula investimentos ao longo prazo");
+            Console.WriteLine("-----------------------------------");
+
+            double fatorRendimento = 1.0036;
+            double valorInvestido = 1000;
+
+            for(int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for(int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+                fatorRendimento += 0.0010;
+            }
+            Console.WriteLine("Ao término do investimento, você terá: R$" + valorInvestido);
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Execução Finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
