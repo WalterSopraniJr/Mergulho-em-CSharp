23d2c5aafe1f2fc73d0e2e4b9fb943ed11cdad28
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-05-14 19:04:53 -0300

    Utilizando laços de repetição 'for' no projeto 11.

---
+++ P11-CalculaPoupanca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P11_CalculaPoupanca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando Projeto 11 - Laços com For");
+            Console.WriteLine("-----------------------------------");
+
+            double valorInvestido = 1000;
+
+            for(int contadorMes = 1; contadorMes <= 12; contadorMes++) 
+            {
+                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
+
+            }
+
+            Console.WriteLine("-----------------------------------");
+            Console.WriteLine("Execução Finalizada. Tecle enter para sair...");
+            Console.ReadLine();
+        }
+    }
+}
