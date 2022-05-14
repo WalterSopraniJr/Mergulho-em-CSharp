07dc25562e63a24ae0e745d590a600b482a5ae99
Walter Soprani Junior <waltersopranijr@gmail.com>
2022-05-14 19:07:01 -0300

    Deixando o calculo mais legível.

--- P11-CalculaPoupanca/Program.cs
+++ P11-CalculaPoupanca/Program.cs
@@ -17,7 +17,7 @@ namespace P11_CalculaPoupanca
 
             for(int contadorMes = 1; contadorMes <= 12; contadorMes++) 
             {
-                valorInvestido = valorInvestido + valorInvestido * 0.0036;
+                valorInvestido *= 1.0036;
                 Console.WriteLine("Após " + contadorMes + " meses, você terá R$" + valorInvestido);
 
             }
